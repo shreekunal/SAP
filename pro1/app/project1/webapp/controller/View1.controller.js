db5cef61bdbb8efdb316b0142d9ef28f976ab063
shrik <shrii.kunal@gmail.com>
2026-02-03 12:10:03 +0530

    add view1

--- pro1/app/project1/webapp/controller/View1.controller.js
+++ pro1/app/project1/webapp/controller/View1.controller.js
@@ -1,10 +1,35 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], (Controller, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("project1.controller.View1", {
         onInit() {
+        },
+
+        onSearch: function (oEvent) {
+            var sQuery = oEvent.getParameter("query") || oEvent.getParameter("newValue");
+            var aFilters = [];
+
+            if (sQuery) {
+                aFilters.push(new Filter({
+                    filters: [
+                        new Filter("orderNo", FilterOperator.Contains, sQuery),
+                        new Filter("customerName", FilterOperator.Contains, sQuery)
+                    ],
+                    and: false
+                }));
+            }
+
+            var oTable = this.byId("ordersTable");
+            if (oTable) {
+                var oBinding = oTable.getBinding("items");
+                if (oBinding) {
+                    oBinding.filter(aFilters);
+                }
+            }
         }
     });
 });
