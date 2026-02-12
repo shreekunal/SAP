948023f97d07717174ca68f3cbd745fc10a084d7
shrik <shrii.kunal@gmail.com>
2026-02-12 11:22:48 +0530

    refactor: streamline security checks in View1.controller.js and update View1.view.xml layout

--- Backend2Backend Connection/pro4/srv/cat-service.cds
+++ Backend2Backend Connection/pro4/srv/cat-service.cds
@@ -1,8 +1,14 @@
 using {my.orders} from '../db/schema';
 
 service OrdersService @(requires: 'authenticated-user') {
+  @(restrict: [
+    { grant: 'READ',   to: 'Read'   },
+    { grant: 'CREATE', to: 'Create' },
+    { grant: 'UPDATE', to: 'Update' },
+    { grant: 'DELETE', to: 'Delete' }
+  ])
   entity Orders as projection on orders.Orders;
-  action securityAction()                                                returns String;
+  function securityAction()                                              returns String;
   action getData()                                                       returns String;
   action createOrder(OrderNo: String, Amount: Decimal, Currency: String) returns String;
 }
--- project1/webapp/controller/View1.controller.js
+++ project1/webapp/controller/View1.controller.js
@@ -1,50 +1,10 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel",
-    "sap/m/MessageToast"
-], (Controller, JSONModel, MessageToast) => {
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
     "use strict";
 
     return Controller.extend("project1.controller.View1", {
         onInit() {
-            const oViewModel = new JSONModel({
-                rolesText: "",
-                hasReadAccess: false
-            });
-            this.getView().setModel(oViewModel, "viewModel");
-
-            // Auto-check roles on load
-            this.getView().attachAfterRendering(this._checkRolesOnLoad.bind(this));
-        },
-
-        _checkRolesOnLoad() {
-            this.getView().detachAfterRendering(this._checkRolesOnLoad, this);
-            this.onCheckSecurityRoles();
-        },
-
-        onCheckSecurityRoles() {
-            const oModel = this.getView().getModel();
-            const oAction = oModel.bindContext("/securityAction(...)");
-
-            oAction.execute()
-                .then(() => {
-                    const oResult = oAction.getBoundContext().getObject();
-                    const oRoles = JSON.parse(oResult.value);
-
-                    const sRolesText = Object.entries(oRoles)
-                        .map(([key, val]) => key + ": " + (val ? "\u2705" : "\u274c"))
-                        .join("  |  ");
-
-                    const oViewModel = this.getView().getModel("viewModel");
-                    oViewModel.setProperty("/rolesText", sRolesText);
-                    oViewModel.setProperty("/hasReadAccess", oRoles.Read === true);
-
-                    MessageToast.show("Security roles loaded");
-                })
-                .catch((err) => {
-                    console.error("Error fetching security roles:", err);
-                    MessageToast.show("Failed to fetch security roles");
-                });
         }
     });
 });
