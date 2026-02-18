9bab804437a2d04c6d8a7fb11276a509b533ee14
shrik <shrii.kunal@gmail.com>
2026-02-18 13:36:14 +0530

    Adjusted gitignore files

--- pro3/app/project1/webapp/controller/Home.controller.js
+++ pro3/app/project1/webapp/controller/Home.controller.js
@@ -10,6 +10,44 @@ sap.ui.define([
         },
 
         /**
+         * Toggle Side Navigation Panel
+         */
+        onSideNavButtonPress: function () {
+            var oToolPage = this.byId("toolPage");
+            var bSideExpanded = oToolPage.getSideExpanded();
+            oToolPage.setSideExpanded(!bSideExpanded);
+        },
+
+        /**
+         * Handle Side Navigation Item Selection
+         */
+        onItemSelect: function (oEvent) {
+            var oItem = oEvent.getParameter("item");
+            var sKey = oItem.getKey();
+            var oRouter = this.getAppComponent().getRouter();
+
+            switch (sKey) {
+                case "home":
+                    // Already on home, do nothing
+                    break;
+                case "categories":
+                    oRouter.navTo("CategoriesList");
+                    break;
+                case "products":
+                    oRouter.navTo("ProductsList");
+                    break;
+                case "reviews":
+                    oRouter.navTo("ReviewsList");
+                    break;
+                case "orderlinks":
+                    oRouter.navTo("OrderLinksList");
+                    break;
+                default:
+                    break;
+            }
+        },
+
+        /**
          * Navigate to Categories List (Fiori Elements)
          */
         onNavCategories: function () {
